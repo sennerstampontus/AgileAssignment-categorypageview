36493a588ceba2ff2b024e92e38e8e1831874fee
Pontus Sennerstam <pontus.sennerstam@learnet.se>
2022-05-16 10:55:38 +0200

    Item #85 / Task #108 - Add template class to list categories on page

--- KenkataWebshop.WebClient/Controllers/CategoryController.cs
+++ KenkataWebshop.WebClient/Controllers/CategoryController.cs
@@ -1,14 +1,34 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using KenkataWebshop.Data;
+using Microsoft.AspNetCore.Mvc;
 
 namespace KenkataWebshop.WebClient.Controllers
 {
+    public class CategoryViewModel
+    {
+        public string Category { get; set; }
+        public List<ProductDto> Products { get; set; }
+    }
+  
     public class CategoryController : Controller
     {
 
         [Route("Category")]
         public IActionResult Index()
         {
-            return View();
+
+
+            var categoryViewModels = new List<CategoryViewModel>
+            {
+                new CategoryViewModel{Category = "Men"},
+                new CategoryViewModel{Category = "Women"},
+                new CategoryViewModel{Category = "Kids"},
+                new CategoryViewModel{Category = "Shoes"},
+                new CategoryViewModel{Category = "Hats"},
+                new CategoryViewModel{Category = "Watches"}
+            };
+
+            return View(categoryViewModels);
         }
     }
 }
+
