5b772fc776726c45c2439867149cf792d84c8691
Pontus Sennerstam <pontus.sennerstam@learnet.se>
2022-05-16 11:41:02 +0200

    Item #85 - Add Category API templates

--- KenkataWebshop.WebApi/Controllers/CategoryController.cs
+++ KenkataWebshop.WebApi/Controllers/CategoryController.cs
@@ -1,11 +1,41 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using KenkataWebshop.Data;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 
 namespace KenkataWebshop.WebApi.Controllers
 {
+    
     [Route("api/[controller]")]
     [ApiController]
     public class CategoryController : ControllerBase
     {
+
+        private readonly SqlContext _sqlContext;
+
+        public CategoryController(SqlContext sqlContext)
+        {
+            _sqlContext = sqlContext;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll()
+        {
+            var categories = await _sqlContext.Categories.ToListAsync();
+            
+            return Ok(categories);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var category = await _sqlContext.Categories.FirstOrDefaultAsync(x => x.Id == id);
+            if(category == null)
+            {
+                return NotFound();
+            }
+
+            return Ok(category);
+        }
     }
 }
